451afb366b0998dd4295c9638f0cf541a70e808a
lephuclai <78690640+lephuclai@users.noreply.github.com>
2023-04-02 15:13:25 +0700

    Add files via upload

--- 4gtrace.py
+++ 4gtrace.py
@@ -7,13 +7,13 @@ while True:
         csv_reader = csv.reader(csv_file, delimiter=',')
         line_count = 0
         for row in csv_reader:
+            #skip the first row of csv file
             if line_count == 0:
                 line_count += 1
                 continue
-            DL_bitrate = float(row[12])*0.001 
+            DL_bitrate = float(row[12])*0.001 #convert kbit/s to mbit/s
             #UL_bitrate = row[13]
-            os.system(f'sudo tc qdisc add dev ens33 root tbf rate {DL_bitrate}mbit burst 32kbit latency 400ms')
-            os.system(f'sudo tc qdisc show dev ens33')
-            time.sleep(1)
-            os.system(f'sudo tc qdisc del dev ens33 root')
-            line_count += 1
+            os.system(f'sudo tc qdisc add dev ens33 root tbf rate {DL_bitrate}mbit burst 32kbit latency 400ms') #apply traffic settings
+            os.system(f'sudo tc qdisc show dev ens33') #show current traffic settings
+            time.sleep(1) #delay for 1s
+            os.system(f'sudo tc qdisc del dev ens33 root') #delete traffic settings
